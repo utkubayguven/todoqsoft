f98ce553d058a11ca3dc2ad84eea5ce81d63d240
utkubayguven <79966502+utkubayguven@users.noreply.github.com>
2022-10-18 19:01:10 +0300

    auth

--- todoqsoft/controllers/auth.js
+++ todoqsoft/controllers/auth.js
@@ -71,24 +71,24 @@ exports.postLogin = async(req,res)=>{
   }
 }
 
-// exports.userAuth = (req, res, next) => {
-//   const token = req.cookies.jwt
-//   if (token) {
-//     jwt.verify(token, jwtSecret, (err,decodedToken) => {
-//       if (err) {
-//         return res.status(401).json({ message: "Not authorized" })
-//       } else {
-//         if (decodedToken.role !== "User") {
-//           return res.status(401).json({ message: "Not authorized" })
-//         } else {
-//           next()
-//         }
-//       }
-//     })
-//   } else {
-//     return res.status(401).json({ message: "Not authorized, token not available" })
-//   }
-// }
+exports.userAuth = (req, res, next) => {
+  const token = req.cookies.jwt
+  if (token) {
+    jwt.verify(token, jwtSecret, (err,decodedToken) => {
+      if (err) {
+        return res.status(401).json({ message: "Not authorized" })
+      } else {
+        if (decodedToken.role !== "User") {
+          return res.status(401).json({ message: "Not authorized" })
+        } else {
+          next()
+        }
+      }
+    })
+  } else {
+    return res.status(401).json({ message: "Not authorized, token not available" })
+  }
+}
 
 
 
@@ -98,4 +98,4 @@ exports.postLogin = async(req,res)=>{
 
 
 
-  
+
